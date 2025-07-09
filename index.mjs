7b0ab2c4921f5ecab7ce97976a80085d0ffabc9a
Pr4veenn <praveen.tech.js@gmail.com>
2025-07-09 22:02:31 +0530

    edited

--- index.mjs
+++ index.mjs
@@ -5,6 +5,8 @@ import { signUPN } from './signup.mjs';
 
 const app = express()
 
+const name ={name : "Praveen", place: "Malappuram"}
+
 signUPN();
 app.listen(3000,()=>{
     console.log(`http://localhost:3000`)
